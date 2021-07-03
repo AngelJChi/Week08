13fa1f51c7538e251f8eefa74cf72e9cb2851233
AngelJChi <2009039@upy.edu.mx>
2021-07-03 18:56:06 -0500

    C9_9.6

---
+++ chapter9ex9.6_replace.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+	#include <stdio.h>
+
+void reemplazar(char char_array[]) {
+	int i;
+
+	for (i = 0; i < sizeof(char_array); ++i) {
+		if (char_array[i] == '-') {
+			char_array[i] = '_';
+		}
+	}
+}
+
+int main(void) {
+	int i;
+
+	char characteres[] = { 'f', 'o', 'o', '-', 'b', '_', 'r' };
+	reemmplazar(characteres);
+
+	for (i = 0; i < sizeof(characteres); ++i) {
+		printf("%c\n", characteres[i]);
+	}
+
+	return 0;
+}
