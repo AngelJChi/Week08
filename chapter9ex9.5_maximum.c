b1518b37872153d53b9a2f98aa34846eb3e4ce12
AngelJChi <2009039@upy.edu.mx>
2021-07-03 17:22:33 -0500

    C9_9.5

---
+++ chapter9ex9.5_maximum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+	int high_water_mark(int valores[]) {
+	int marca_alta;
+	int i;
+
+	marca_alta = 0;
+
+	for (i = 0; i < sizeof(valores); ++i) {
+		if (valores[i] > marca_alta) {
+			marca_alta = valores[i];
+		}
+	}
+
+	return marca_alta;
+}
+
+int main(void) {
+	int valores[] = { 3, 5, 7, 58, 910, 4, 64, 3 };
+
+	printf("Resultado: %d\n", high_water_mark(valores));
+	return 0;
+}
