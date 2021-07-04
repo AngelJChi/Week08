9dc5ed58ac7efb72894e139659d2c316068b78ce
AngelJChi <83776637+AngelJChi@users.noreply.github.com>
2021-07-04 17:12:17 -0500

    C9_9.2

--- chapter9ex9.2_strings.c
+++ chapter9ex9.2_strings.c
@@ -4,13 +4,15 @@ int begins(char string1[], char string2[]) {
 	int i = 0;
 
 	while (1) {
-
+		
+		/*Si llegamos al final de la cadena de prueba sin devolver false, 
+		*entonces los bits anteriores deben coincidir. Return true.*/
 
 		if (string1[ i ] == '\0') {
 			return(1);
 		}
 
-
+		/* Si encontramos una falta de coincidencia, return false. De lo contrario, sigue adelante. */
 		if (string1[i] != string2[i]) {
 			return(0);
 		}
